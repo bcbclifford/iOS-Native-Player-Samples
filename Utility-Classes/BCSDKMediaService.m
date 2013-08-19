edb716de36668a6934527b6bbcb0bd1b16335e95
bcbclifford <bclifford@brightcove.com>
2013-08-19 13:02:12 +0100

    Better determine playable URL

--- Utility-Classes/BCSDKMediaService.m
+++ Utility-Classes/BCSDKMediaService.m
@@ -25,8 +25,27 @@
         [properties setValue:[json objectForKey:@"longDescription"] forKey:@"longDescription"];
     }
     
+    BCRendition *rendition = nil;
+    BCRenditionSet *renditionSet = nil;
+    
+    // Check if HLSURL property exists and is not null. If not, use FLVURL instead.
+    // For this to work request HLSURL as a video_field and set media_type to HTTP, e.g.
+    // NSDictionary *apiOptions = [[NSDictionary alloc] initWithObjectsAndKeys: @"HTTP", @"media_delivery", @"FLVFullLength,videoStillURL,name,shortDescription,referenceId,id,customFields,FLVURL,HLSURL", @"video_fields", nil];
+    // catalog = [[BCSDKCatalog alloc] initWithToken:token];
+    // [catalog findVideoByID:videoId options:apiOptions callBlock:^(BCError *error, BCVideo *video) { … }];
+    if([json objectForKey:@"HLSURL"] && ![[json objectForKey:@"HLSURL"] isEqual:[NSNull null]]) {
+        rendition = [[BCRendition alloc] initWithURL:[NSURL URLWithString:[json objectForKey:@"HLSURL"]]];
+        NSLog(@"Video has transcoded HLS");
+    } else {
+        // if no HLS rendition is found, attempt to playback the FLVURL as a normal video.
+        rendition = [[BCRendition alloc] initWithURL:[NSURL URLWithString:[json objectForKey:@"FLVURL"]]];
+        NSLog(@"Video does not have transcoded HLS");
+    }
+    renditionSet = [[BCRenditionSet alloc] initWithRenditions:[NSArray arrayWithObject:rendition]
+                                                deliveryMethod:[NSDictionary dictionary]];
+    
     //Return a newly created video with the new properties
-    return [[BCVideo alloc] initWithRenditionSets:video.renditionSets properties:properties];
+    return [[BCVideo alloc] initWithRenditionSet:renditionSet properties:properties];
 }
 
 @end
